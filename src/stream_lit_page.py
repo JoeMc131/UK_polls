63ea3eabe4472b61885c5adbe1d2fc69dafa8a38
JoeMc131 <joemc131@outlook.com>
2026-06-03 14:02:37 +0100

    updating code

--- src/stream_lit_page.py
+++ src/stream_lit_page.py
@@ -131,8 +131,8 @@ for party in party_cols:
     ds = df_ma.date
 
     input_dict = {
-        'ds':ds,
-        'y':y
+        'ds':ds[-180:],
+        'y':y[-180:]
     }
 
     input_df = pd.DataFrame(input_dict)
